aafffc3d5434f29575a3d322fe1ba268e13fa393
Art-mj1 <mjmjpc@naver.com>
2021-09-01 15:32:58 +0900

    09/01 리액트 상세 수정
    상세페이지 디자인 수정

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,13 +1,21 @@
 import React from "react";
+import "../css/Detail.css";
 
 const Details = (props) => {
   console.log(props);
   const title = props.location.state.title;
   const img = props.location.state.img;
+  const ingrs = props.location.state.ingrs;
   return (
-    <div>
+    <div className='detail'>
       <h2>{title}</h2>
       <img src={img} alt='' />
+      <ul>
+        <h3>Ingredients</h3>
+        {ingrs.map((ingr, i) => (
+          <li key={i}>{ingr.text}</li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -19,11 +19,6 @@ const Recipe = ({ title, calories, img, ingrs }) => {
         </Link>
       </h2>
       <img src={img} alt='' />
-      <ul>
-        {ingrs.map((ingr, i) => (
-          <li key={i}>{ingr.text}</li>
-        ))}
-      </ul>
       <p>칼로리:{calories}</p>
     </div>
   );
