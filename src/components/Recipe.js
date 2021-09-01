7357ac104f5ef7bd40ff19d8266c3568e37cbfba
Art-mj1 <mjmjpc@naver.com>
2021-09-01 11:38:24 +0900

    09/01 리액트 상세페이지로 넘기기
    상세페이지 넘기기

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,10 +1,23 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 //hooks
 const Recipe = ({ title, calories, img, ingrs }) => {
   return (
     <div>
-      <h2>{title}</h2>
+      <h2>
+        <Link
+          to={{
+            pathname: "/details",
+            state: {
+              title: title,
+              calories: calories,
+              img: img,
+              ingrs: ingrs,
+            },
+          }}>
+          {title}
+        </Link>
+      </h2>
       <img src={img} alt='' />
       <ul>
         {ingrs.map((ingr, i) => (
